6ee620a46eb98cc29f3f5d1ea67782e0af0eec5b
Frank Santiesteban <franksantiesteban@Franks-MacBook-Pro.local>
2018-08-16 00:06:22 -0400

    made folders

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Tex', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Toston Burger', false);
